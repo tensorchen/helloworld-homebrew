77643b59189a51dfd8724ea5adacb187d8f72d80
tensorchen <tensorchen@gmail.com>
2020-06-25 16:49:20 +0800

    chore: add homebrew support

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	fmt.Println("Hello, Homebrew!")
+	fmt.Printf("version %s, commit %s, built at %s\n", version, commit, date)
 }
